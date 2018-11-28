f7cea5737063183d3c2ab549ccc9a47a255f1aeb
Donald Fossouo <dfossouo@hortonworks.com>
2018-11-28 17:50:53 +0100

    v1.0 - this first release contain structure of project v1.0.1 - Edition of README.md
    v1.final the code is ready to use need to have csv input file in hdfs directory containing the bp of the customer

--- src/main/scala/com/github/dfossouo/SparkHBase/GDPRHBase.scala
+++ src/main/scala/com/github/dfossouo/SparkHBase/GDPRHBase.scala
@@ -4,14 +4,15 @@ This code does the following:
   2) Extract specific time Range
   3) Create two dataframes and compare them to give back lines where the tables are differents (only the columns where we have differences)
 Usage:
-SPARK_MAJOR_VERSION=2 spark-submit --class com.github.dfossouo.SparkHBase.SparkReadHBaseTable_DiscoverSchema --master yarn --deploy-mode client --driver-memory 1g --executor-memory 4g --executor-cores 1 --jars ./target/HBaseCRC-0.0.2-SNAPSHOT.jar /usr/hdp/current/phoenix-client/phoenix-client.jar /tmp/props2
+SPARK_MAJOR_VERSION=2 spark-submit --class com.github.dfossouo.SparkHBase.GDPRHBase --master yarn --deploy-mode client --driver-memory 2g --executor-memory 2g --executor-cores 1 --num-executors 2 --jars ./target/GDPRHBASE-0.0.2-SNAPSHOT.jar /usr/hdp/current/phoenix-client/phoenix-client.jar ./props
 NB: props2 est un fichier qui contient les variables d'entrées du projet
   ********************************************************************************************************/
 
-package main.scala.com.github.dfossouo.SparkHBase
+package com.github.dfossouo.SparkHBase
 
 import java.util.Calendar
 
+import org.apache.hadoop.hbase.{CellUtil, HBaseConfiguration, TableName}
 import org.apache.hadoop.hbase.client._
 import org.apache.hadoop.hbase.protobuf.ProtobufUtil
 import org.apache.hadoop.hbase.util.Base64
@@ -19,6 +20,9 @@ import org.apache.spark.sql.execution.datasources.hbase._
 import org.apache.spark.sql.functions._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.hadoop.hbase.util.{Base64, Bytes}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 import scala.collection.mutable.HashMap
 import scala.io.Source.fromFile
@@ -27,6 +31,15 @@ import scala.io.Source.fromFile
 object GDPRHBase {
 
   case class hVar(rowkey: Int, colFamily: String, colQualifier: String, colDatetime: Long, colDatetimeStr: String, colType: String, colValue: String)
+
+  // schema for sensor data
+  case class reference(rowkey: String, level: String, bp: String, pdl: String, custid: String) extends Serializable
+
+  def customer(str: String): reference = {
+    val p = str.split(",")
+    reference(p(0), p(1), p(2), p(3), p(4))
+  }
+
 
   def main(args: Array[String]) {
 
@@ -50,7 +63,10 @@ object GDPRHBase {
     val quorum = props.get("hbase.zookeeper.quorum").get
 
     // Create Spark Application
-    val sparkConf = new SparkConf().setAppName("SparkReadHBaseTable")
+    val sparkConf = new SparkConf().setAppName("GDPRHBASE")
+    sparkConf.set("spark.driver.allowMultipleContexts", "true")
+    sparkConf.set("spark.shuffle.service.enabled", "false")
+    sparkConf.set("spark.dynamicAllocation.enabled", "false")
 
     val sc = new SparkContext(sparkConf)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
@@ -69,7 +85,10 @@ object GDPRHBase {
         "rowkey":"key",
         "columns":{
         "rowkey":{"cf":"rowkey", "col":"key", "type":"string"},
-        "data":{"cf":"demographics", "col":"", "type":"map<string, string>"}
+        "level":{"cf":"demographics", "col":"level", "type":"string"},
+        "bp":{"cf":"demographics", "col":"bp", "type":"string"},
+        "pdl":{"cf":"demographics", "col":"pdl", "type":"string"},
+        "custid":{"cf":"demographics", "col":"custid", "type":"string"}
         }
         }""".stripMargin
 
@@ -87,7 +106,7 @@ object GDPRHBase {
     def withCatalogInfo(table_cluster: String): DataFrame = {
       sqlContext
         .read
-        .options(Map(HBaseTableCatalog.tableCatalog->table_cluster,HBaseRelation.RESTRICTIVE -> "HBaseRelation.Restrictive.none",HBaseRelation.MIN_STAMP -> "0", HBaseRelation.MAX_STAMP -> "15416709729711".toString, HBaseRelation.HBASE_CONFIGURATION -> connectionHbase))
+        .options(Map(HBaseTableCatalog.tableCatalog->table_cluster, HBaseRelation.HBASE_CONFIGURATION -> connectionHbase))
         .format("org.apache.spark.sql.execution.datasources.hbase")
         .load()
     }
@@ -103,13 +122,44 @@ object GDPRHBase {
     df.columns.map(f => print(f))
     df.show(10,false)
 
-    val ssc = new StreamingContext(sparkConf, Seconds(10))
+    val ssc = new StreamingContext(sc, Seconds(10))
 
-    df.foreach(row => print(row))
-    /*   val colNames = Seq("rowkey", "key1", "key2")
-       val newDF = dfjoin.toDF(colNames: _*)
-      val df_difference_columns = diff_row("rowkey","key1", "key2",newDF)
-       df_difference_columns.show(false) */
+    df.write.format("csv").mode("overwrite").save("/user/hbase/df.csv")
+
+    val schema =new StructType().add(StructField("bp",StringType,true))
+      .add(StructField("pdl",IntegerType,true))
+      .add(StructField("custid",StringType,true))
+      .add(StructField("level",StringType,true))
+
+    // parse the lines of data into customer objects
+    val textDStream = ssc.textFileStream("/user/hbase/customers_deletion.csv")
+    val schema2 =new StructType().add(StructField("bp",StringType,true)).add(StructField("custid",StringType,true)).add(StructField("level",StringType,true)).add(StructField("pdl",StringType,true))
+
+    println("********" + "print each elements")
+    val hConf = HBaseConfiguration.create()
+    hConf.setInt("timeout", 120000)
+    hConf.set("hbase.rootdir", props.getOrElse("hbase.rootdir", "/tmp"))
+    hConf.set("zookeeper.znode.parent", props.getOrElse("zookeeper.znode.parent", "/hbase-unsecure"))
+    hConf.set("hbase.zookeeper.quorum", props.getOrElse("hbase.zookeeper.quorum", "hdpcluster-15377-master-0.field.hortonworks.com:2181"))
+    val hTable = new HTable(hConf,"clients_info_28112018")
+    val cfPersonalData = Bytes.toBytes("demographics")
+    val iban = Bytes.toBytes("iban")
+    val street = Bytes.toBytes("street")
+    // Create Connection
+    val connection: Connection = ConnectionFactory.createConnection(hConf)
+
+    textDStream.foreachRDD(rowsRDD => {
+      val bpcol = rowsRDD.map(x => x.split(",")).map(x => Row(x(0)))
+      bpcol.collect().foreach(i => {
+        val record = i.mkString
+        val put = new Put(Bytes.toBytes(record))
+        put.add(cfPersonalData,iban,Bytes.toBytes("FRXX 2002 XXXX 2606 XXXX YYYY 606"))
+        put.add(cfPersonalData,street,Bytes.toBytes("XX RUE DE PARIS 750XX PARIS"))
+        hTable.put(put)
+
+      })
+    })
+
 
     // Start the computation
     ssc.start()
@@ -137,4 +187,4 @@ object GDPRHBase {
 
 
 }
-//ZEND
+//WAKANDA FORVER
